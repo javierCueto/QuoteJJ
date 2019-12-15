ec0c2f84226be216767aa151634f02be1cd0d9bd
Javier <javelie@outlook.com>
2019-12-15 16:49:25 -0700

    Initial Commit

---
+++ QuoteJJ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuoteJJ
+//
+//  Created by José Javier Cueto Mejía on 15/12/19.
+//  Copyright © 2019 José Javier Cueto Mejía. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
